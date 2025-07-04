5a0fc87c44d1d9d5994d78a3fc7df42d9f49d37c
Ambuj Mahato <ambujmahato97322@gmail.com>
2025-07-04 12:27:26 +0530

    url linking used

--- myapp/views.py
+++ myapp/views.py
@@ -1,33 +1,41 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# # Create your views here.
+# def app(request):
+#     return HttpResponse("Hello Asur")
+
+# def dynamic_response(request,string):
+#     return HttpResponse(f"{string}")
+
+# def factvw(request,no):
+#     fact=1
+#     for i in range(1,no+1):
+#         fact =fact*i
+#     return HttpResponse(f"{fact}")
+
+
+# def length(request,string):
+#     length=len(string)
+#     return HttpResponse(f"{length}")
+
+# def find(request,string,char):
+#     count=0
+#     for i in range(len(string)):
+#         if string[i]==char:
+#             count+=1
+#     return HttpResponse(f"{count}")
+
+# def indexvw(request,a):
+#     print(a)
+#     return render(request,'index.html',{'a':a})
+
+# def avw(request):
+#     return render(request,'a.html')
+
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def app(request):
-    return HttpResponse("Hello Asur")
 
-def dynamic_response(request,string):
-    return HttpResponse(f"{string}")
+def indexVw(request):
+   return render(request,'index.html')
 
-def factvw(request,no):
-    fact=1
-    for i in range(1,no+1):
-        fact =fact*i
-    return HttpResponse(f"{fact}")
-
-
-def length(request,string):
-    length=len(string)
-    return HttpResponse(f"{length}")
-
-def find(request,string,char):
-    count=0
-    for i in range(len(string)):
-        if string[i]==char:
-            count+=1
-    return HttpResponse(f"{count}")
-
-def indexvw(request,a):
-    print(a)
-    return render(request,'index.html',{'a':a})
-
-def avw(request):
-    return render(request,'a.html')
+def ContactVw(request):
+   return render(request,'contact.html')
--- rms/urls.py
+++ rms/urls.py
@@ -14,15 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+# from myapp import views
+# urlpatterns = [
+#     path('hello',views.app),
+#     path('dyn_res/<str:string>',views.dynamic_response),
+#     path('fact/<int:no>',views.factvw),
+#     path('length/<str:string>',views.length),
+#     path('find/<str:string>/<str:char>',views.find),
+#     path('index/<int:a>',views.indexvw),
+#     path('a',views.avw),
+# ]
+
 from django.urls import path
 from myapp import views
 urlpatterns = [
-    path('hello',views.app),
-    path('dyn_res/<str:string>',views.dynamic_response),
-    path('fact/<int:no>',views.factvw),
-    path('length/<str:string>',views.length),
-    path('find/<str:string>/<str:char>',views.find),
-    path('index/<int:a>',views.indexvw),
-    path('a',views.avw),
+   path('index',views.indexVw,name='index'),
+   path('contact',views.ContactVw,name='contact'),
 ]
